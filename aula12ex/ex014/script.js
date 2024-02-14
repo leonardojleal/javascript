76579337386f568fb319fb7d53b2c705446f9077
Leonardo Leal <l.leal14@hotmail.com>
2024-02-14 19:47:14 -0300

    atualização ex014

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,24 @@
-alert('Olá!')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+
+    var hora = 23
+
+    
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora <12){
+        //BOM DIA!
+        img.src = 'manha250.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18){
+        //BOA TARDE!
+        img.src = 'tarde250.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        //BOA NOITE!
+        img.src = 'noite250.png'
+        document.body.style.background = '#515154'
+    }
+}
