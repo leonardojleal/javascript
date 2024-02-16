adf919b709d0ed58fa70ed6f86d8c85a9b98a4e9
Leonardo Leal <l.leal14@hotmail.com>
2024-02-15 21:02:17 -0300

    criando aula013

---
+++ aula013/ambiente.js
@@ -0,0 +1,33 @@
+/*REPETIÇÃO TESTE LÓGICO NO FINAL*/
+
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+} while(c <= 2)
+
+
+
+/*
+REPETIÇÃO TESTE LÓGICO NO INÍCIO
+var c = 1
+while(c <= 10){
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*console.log('Tudo bem?')*/
+
